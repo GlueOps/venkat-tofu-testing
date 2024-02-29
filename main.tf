802f32909f825333055103434a14e23298fb65ad
Venkata Mutyala <venkata.mutyala@glueops.dev>
2024-02-29 15:06:56 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,8 +11,27 @@ resource "random_integer" "priority" {
 
 
 
+terraform {
+  required_providers {
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "cloudflare" {
+ 
+}
 
 
+resource "cloudflare_record" "fake-domain-record" {
+  zone_id = "ad02d78e42f142839dca4e63a1929097"
+  name    = "yolooooooooo"
+  value   = "8.8.8.8"
+  type    = "A"
+  ttl     = 3600
+}
 
 
 
