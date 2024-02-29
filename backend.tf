b867b71ae4fe2b136f4969f9a7b951879c9e5e48
Venkata Mutyala <venkata.mutyala@glueops.dev>
2024-02-29 14:31:05 -0800

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = venkata-test-bucket"
+    bucket         = "venkata-test-bucket"
     key            = "glueops/venkata-tofu-testing/terraform.tfstate"
     region         = "us-west-2"
   }
