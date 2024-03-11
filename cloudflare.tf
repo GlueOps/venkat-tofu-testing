f14beedad00ce411ca5c4c8c64e8b679404438f2
Venkata Mutyala <venkata.mutyala@glueops.dev>
2024-03-11 09:05:04 -0700

    Update cloudflare.tf

--- cloudflare.tf
+++ cloudflare.tf
@@ -3,38 +3,6 @@
 #  }
 #}
 
-resource "random_integer" "priority" {
-  min = 1
-  max = 500
-}
-
-
-
-
-terraform {
-  required_providers {
-    cloudflare = {
-      source  = "cloudflare/cloudflare"
-      version = "~> 4.0"
-    }
-  }
-}
-
-provider "cloudflare" {
-
-}
-
-
-resource "cloudflare_record" "fake-domain-record" {
-  zone_id = "ad02d78e42f142839dca4e63a1929097"
-  name    = "yolo-tofu"
-  value   = "8.8.8.8"
-  type    = "A"
-  ttl     = 3600
-}
-
-
-
 
 
 
