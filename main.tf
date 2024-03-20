21eb2912f9c9892fbb95fd2fea8a89f51b93cd25
Venkata Mutyala <venkata.mutyala@glueops.dev>
2024-03-20 08:59:44 -0700

    feat: demo for wes

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 resource "random_integer" "demo" {
-  for_each = { for i in range(250) : tostring(i) => i }
+  for_each = { for i in range(249) : tostring(i) => i }
 
   min = 1
-  max = 500
+  max = 100
 }
 
 
@@ -12,4 +12,12 @@ resource "random_integer" "demo2" {
 
   min = 1
   max = 500
+}
+
+
+resource "random_integer" "demo3" {
+  for_each = { for i in range(1) : tostring(i) => i }
+
+  min = 1
+  max = 00
 }
